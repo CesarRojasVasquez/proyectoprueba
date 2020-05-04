e97ade4b76f9bd08eee14f946ecefa06f144a258
CesarRojasVasquez <64751808+CesarRojasVasquez@users.noreply.github.com>
2020-05-03 20:41:46 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body>
+    <h1>Bienvenido </h1>
+    <a href="pagina.php">Ventana 2</a>
+</body>
+
+</html>
